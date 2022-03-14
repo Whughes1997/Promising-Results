f0cc05bb6934f47d86a0fdfa7ae306a3b03a5176
Will Hughes <killjoyfpss@wills-mbp.lan>
2022-03-14 14:42:48 -0500

    commit message

---
+++ app.js
@@ -0,0 +1,15 @@
+// slowMath.add(2, 6).then(val => console.log(val))
+// slowMath.multiply(8, 2).then(val => console.log(val))
+// slowMath.divide(16, 4).then(val => console.log(val))
+// slowMath.add(4, 96).then(val => console.log(val))
+// slowMath.divide(100, 2).then(val => console.log(val))
+// slowMath.multiply(50, 50).then(val => console.log(val))
+// slowMath.divide(2500, 40).then(val => console.log(val))
+// slowMath.add(32, 62.5).then(val => console.log(val))
+//     .catch(err => console.log(err));
+// let result = 94.5;
+
+function doMath() {
+    return console.log("42")
+}
+doMath()
